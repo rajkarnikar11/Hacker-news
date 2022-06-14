78d8cc93093dc3d1524380ebd8435b28b842c5f7
rajkarnikar11 <rajkarnikarsalil11@gmail.com>
2022-06-14 15:31:39 +0545

    navbar completed

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-	<Navbar />
-	<router-view />
+	<div class="home__container">
+		<Navbar />
+		<router-view />
+	</div>
 </template>
 
 <script>
@@ -10,4 +12,19 @@ export default {
 	components: { Navbar },
 };
 </script>
-<style></style>
+<style>
+* {
+	box-sizing: border-box;
+	margin: 0;
+	padding: 0;
+}
+body {
+	font-family: sans-serif;
+	margin: 10px;
+}
+.home__container {
+	max-width: 1280px;
+	margin: auto;
+	background-color: #f7f7ef;
+}
+</style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,23 @@
 <template>
 	<nav>
-		<router-link to="/">Home</router-link> |
-		<router-link to="/about">About</router-link>
+		<div class="nav__left">
+			<router-link class="title__container" to="/">
+				<h3 class="title__logo">Y</h3>
+				<h1 class="title__name">Hacker News</h1>
+			</router-link>
+			<router-link class="nav__link" to="/newest">new</router-link>
+			<router-link class="nav__link" to="/front">past</router-link>
+			<router-link class="nav__link" to="/newcomments"
+				>comments</router-link
+			>
+			<router-link class="nav__link" to="/ask">ask</router-link>
+			<router-link class="nav__link" to="/show">show</router-link>
+			<router-link class="nav__link" to="/jobs">jobs</router-link>
+			<router-link class="nav__link" to="/submit">submit</router-link>
+		</div>
+		<div class="nav__right">
+			<router-link class="nav__link" to="/submit">login</router-link>
+		</div>
 	</nav>
 </template>
 
@@ -11,4 +27,58 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+nav {
+	background: #ff6600;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	height: 30px;
+	padding: 2px;
+}
+.nav__left .nav__link {
+	border-right: 1px solid black;
+	padding-right: 10px;
+	height: 18 px;
+}
+.title__container {
+	display: flex;
+	text-decoration: none;
+	align-items: center;
+}
+.nav__left {
+	display: flex;
+	width: 45%;
+	justify-content: space-between;
+	align-items: center;
+}
+.nav__link {
+	text-decoration: none;
+	color: black;
+}
+
+.title__name {
+	font-size: 18px;
+	line-height: 25px;
+	color: black;
+}
+.nav__right {
+	margin-right: 5px;
+}
+.title__logo {
+	color: white;
+	font-size: 15px;
+	line-height: 25px;
+	justify-content: center;
+	align-items: center;
+	margin-right: 10px;
+	display: flex;
+	border: 2px solid white;
+	height: 20px;
+	box-sizing: border-box;
+	width: 20px;
+}
+.router-link-exact-active {
+	color: white;
+}
+</style>
